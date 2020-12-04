110077eee3cb2d43b7b232157ef4d808f109428e
emine-celik <emineclk80@hotmail.com>
2020-12-03 21:31:06 -0600

    YENI SORULAR

---
+++ src/OCA_QA_Practice/Q153.java
@@ -0,0 +1,27 @@
+package OCA_QA_Practice;
+
+public class Q153 {
+
+    //public	class Fieldint{
+    char c;
+    boolean b;
+    float f;
+    void printAll() {
+        System.out.println("c= "+c);//c=
+        System.out.println("b= "+b);//b=false
+        System.out.println("f= "+f);//f=0.0
+    }
+
+
+
+    //	public class Q147 {
+
+
+    public static void main(String[] args) {
+        Q153 f=new Q153();
+        f.printAll();
+    }
+
+}
+
+
